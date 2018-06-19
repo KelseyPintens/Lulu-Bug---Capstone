ba86267de164631f749e52c9d92c00025e623bbb
Kelsey Pintens <kelseypintens@Kelseys-MacBook-Pro.local>
2018-06-19 18:45:29 -0500

    experimenting with camera

--- src/components/TakePic.js
+++ src/components/TakePic.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 import '../App.css';
+import ReactFileReader from 'react-file-reader';
 
 
 class Pic extends Component {
 
-    previewFile = () => {
-        var preview = document.querySelector('img');
-        var file    = document.querySelector('input[type=file]').files[0];
-        var reader  = new FileReader();
-      
-        reader.addEventListener("load", function () {
-          preview.src = reader.result;
-        }, false);
-      
-        if (file) {
-          reader.readAsDataURL(file);
-        }
+    handleFiles = files => {
+        console.log(files)
       }
+
+    // previewFile = () => {
+    //     var preview = document.querySelector('img');
+    //     var file    = document.querySelector('input[type=file]').files[0];
+    //     var reader  = new FileReader();
+      
+    //     reader.addEventListener("load", function () {
+    //       preview.src = reader.result;
+    //     }, false);
+      
+    //     if (file) {
+    //       reader.readAsDataURL(file);
+    //     }
+    //   }
 
     render() {
         return (
-            <div className="col ml-3">
-<input type="file" onchange={this.previewFile}/>
-<img src="" height="200" alt="Image preview..."/>
 
-            </div>
+            <ReactFileReader handleFiles={this.handleFiles}>
+  <button className='btn'>Upload</button>
+</ReactFileReader>
+//             <div className="col ml-3">
+// <input type="file" onchange={this.previewFile}/>
+// <img src="" height="200" alt="Image preview..."/>
+
+//             </div>
         );
     }
 }
